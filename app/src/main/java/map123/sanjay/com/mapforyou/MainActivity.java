2468417f8caf33fdf372495318662135de8c97e2
Sanjay YM <sanjayspear@gmail.com>
2015-08-14 14:25:54 +0530

    My commit for simple google map

---
+++ app/src/main/java/map123/sanjay/com/mapforyou/MainActivity.java
@@ -0,0 +1,53 @@
+package map123.sanjay.com.mapforyou;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    static  final LatLng Banglore = new LatLng(12.9667, 77.5667);
+    private GoogleMap map;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        map = ((MapFragment)getFragmentManager().findFragmentById(R.id.map)).getMap();
+        Marker bang = map.addMarker(new MarkerOptions().position(Banglore).title("Banglore").snippet("Banglore is an amazing IT World"));
+        map.moveCamera(CameraUpdateFactory.newLatLngZoom(Banglore, 15));
+        map.animateCamera(CameraUpdateFactory.zoomTo(10),2000,null);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
